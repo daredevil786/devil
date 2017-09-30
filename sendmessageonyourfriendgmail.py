63ca404c112e177497b18e94072d09ab560a4532
amrendra singh rathore <31726307+daredevil786@users.noreply.github.com>
2017-09-30 10:41:25 +0530

    Update and rename frrr.py to sendmessageonyourfriendgmail.py

--- sendmessageonyourfriendgmail.py
+++ sendmessageonyourfriendgmail.py
@@ -3,6 +3,6 @@ import smtplib
 s=smtplib.SMTP('smtp.gmail.com',587)	
 s.starttls()
 s.login("theprotectorofsword@gmail.com","9934816772")
-message="yo"
-s.sendmail("theprotectorofsword@gmail.com","theprotectorofsword@gmai.com",message)
+message="hiiii rahul"
+s.sendmail("theprotectorofsword@gmail.com","rudratheburningfire@gmail.com",message)
 s.quit()
