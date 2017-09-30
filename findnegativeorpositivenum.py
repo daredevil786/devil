2eff1c9e6c568bcb8ec79ac7fc0626a620feb9ec
amrendra singh rathore <31726307+daredevil786@users.noreply.github.com>
2017-09-30 16:34:00 +0530

    Update findnegativeorpositivenum.py

--- findnegativeorpositivenum.py
+++ findnegativeorpositivenum.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-n=int(input("enter a number"))
-if n==0:
+n=int(input("enter a number")) #any numerical value
+if n==0: #condition for zero
   print("zero") 
-elif n<0:
+elif n<0: #condition for negative
   print("negative")
 else:
-  print("positive")
+  print("positive") 
