a6ae594ac05e8316d34d4fcab4546323413c6975
amrendra singh rathore <31726307+daredevil786@users.noreply.github.com>
2017-09-30 16:19:17 +0530

    Update calculation.py

--- calculation.py
+++ calculation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 a=77
 print(a)
-print(a+5)
-print(a-11)
-print(a*12)
-print(a%5)
+print(a+5) #addition
+print(a-11) #subtraction
+print(a*12) #multiplication
+print(a%5)  #division
