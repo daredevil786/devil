35930e2aadc1aa6890d74f8c4d10c04a254c39ff
amrendra singh rathore <31726307+daredevil786@users.noreply.github.com>
2017-09-30 16:36:03 +0530

    Update gamestonepaperandscissors.py

--- gamestonepaperandscissors.py
+++ gamestonepaperandscissors.py
@@ -1,20 +1,20 @@
 import random
-r=["stone","paper","scissors"]
+r=["stone","paper","scissors"] #range
 computer=r[random.randint(0,2)]
 player=input("enter the choice");
 if player==computer:
       print("draw")
-elif player=="paper":
+elif player=="paper": #condition for paper outcome
       if computer=="scissors":
           print("lose","computer has",computer)
       else:
           print("won","computer has",computer)
-elif player=="stone":
+elif player=="stone": #condition for stone outcome
       if computer=="paper":
           print("lose","computer has",computer)
       else:
           print("won","computer has",computer)
-elif player=="scissors":
+elif player=="scissors": #condition for scissors outcome
       if computer=="paper":
          print("lose","computer has",computer)
       else:
