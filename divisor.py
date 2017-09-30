25feb7fdcf65fd7dc0a41ca0b5587fd1c38d828c
amrendra singh rathore <31726307+daredevil786@users.noreply.github.com>
2017-09-30 16:28:45 +0530

    Update divisor.py

--- divisor.py
+++ divisor.py
@@ -1,6 +1,6 @@
-n=int(input("enter a integer:"))
-print("divisors list is")
-for i in range(1,n+1):
+n=int(input("enter a integer:")) #any numerical value
+print("divisors list is") 
+for i in range(1,n+1): #range how much divisors you want
     if(n%i==0):
      print(i)
    
