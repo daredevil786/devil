8030879a4e0520910ef9b4c84d8a5eb6b83e67c9
amrendra singh rathore <31726307+daredevil786@users.noreply.github.com>
2017-10-02 12:20:18 +0530

    Update sendmessageonyourfriendgmail.py

--- sendmessageonyourfriendgmail.py
+++ sendmessageonyourfriendgmail.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 import smtplib
-s=smtplib.SMTP('smtp.gmail.com',587)	
+s=smtplib.SMTP('smtp.gmail.com',587) #condition for sending mail	
 s.starttls()
-s.login("theprotectorofsword@gmail.com","9934816772")
-message="hiiii rahul"
-s.sendmail("theprotectorofsword@gmail.com","rudratheburningfire@gmail.com",message)
+s.login("theprotectorofsword@gmail.com","9934816772") # write your gmail and password
+message="hiiii rahul" #write message which you want to send
+s.sendmail("theprotectorofsword@gmail.com","rudratheburningfire@gmail.com",message) # write your mail and your friend mail
 s.quit()
