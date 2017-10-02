b7f072f3bfc54b1f376b83b04ac6697e3bcd7528
amrendra singh rathore <31726307+daredevil786@users.noreply.github.com>
2017-10-02 12:24:03 +0530

    Update table2to6.py

--- table2to6.py
+++ table2to6.py
@@ -1,2 +1,2 @@
-for i in range(1,11):
-	print (i*2,'\t',i*3,'\t',i*4,'\t',i*5,'\t',i*6,'\t')
+for i in range(1,11): #range
+	print (i*2,'\t',i*3,'\t',i*4,'\t',i*5,'\t',i*6,'\t') #condition for write table
