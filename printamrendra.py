a323cca61e9d7765727bfdcae1826c50dcae319b
amrendra singh rathore <31726307+daredevil786@users.noreply.github.com>
2017-10-02 12:12:40 +0530

    Update printamrendra.py

--- printamrendra.py
+++ printamrendra.py
@@ -1,6 +1,6 @@
-a="amrendra"
+a="amrendra" #word which we want to print
 print(a)
-print(a+"singh")
-print(a[4])
+print(a+"singh") #condition for adding another word 
+print(a[4])  #condition for repeat a word 
 print(a*4)
 print(a[3])
