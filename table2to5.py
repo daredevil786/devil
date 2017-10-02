5acd32487904b8c9e0766a451c258cbb713b86f3
amrendra singh rathore <31726307+daredevil786@users.noreply.github.com>
2017-10-02 12:23:14 +0530

    Update table2to5.py

--- table2to5.py
+++ table2to5.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for i in range(1,11):
-	print (i*2,'\t',i*3,'\t',i*4,'\t',i*5,'\t',)
+for i in range(1,11): #range
+	print (i*2,'\t',i*3,'\t',i*4,'\t',i*5,'\t',) #conditon for write table
