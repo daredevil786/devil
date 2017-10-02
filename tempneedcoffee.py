168acb069a25f0d6ce8006ee679822e463b34ed8
amrendra singh rathore <31726307+daredevil786@users.noreply.github.com>
2017-10-02 12:29:31 +0530

    Update tempneedcoffee.py

--- tempneedcoffee.py
+++ tempneedcoffee.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 temperture=12 
 while temperture<20:
-	temp=input("are u cole: y/n")
+	temp=input("are u cole: y/n") #condition for temp
 	print(temperture)
 	temperture=temperture+1
 	if temp=="y":
-	print("need coffee")
+	print("need coffee") #output
