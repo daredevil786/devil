3303bce79cb089179453121b2e7efc2fddcb1085
amrendra singh rathore <31726307+daredevil786@users.noreply.github.com>
2017-10-02 12:22:17 +0530

    Update sendmessageonyourgmail.py

--- sendmessageonyourgmail.py
+++ sendmessageonyourgmail.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 import smtplib
-s=smtplib.SMTP('smtp.gmail.com',587)	
+s=smtplib.SMTP('smtp.gmail.com',587)	# condition for sending mail
 s.starttls()
-s.login("rudratheburningfire@gmail.com","9097362269")
-message="yo"
-s.sendmail("rudratheburningfire@gmail.com@gmail.com","rudratheburningfire@gmail.com",message)
+s.login("rudratheburningfire@gmail.com","9097362269") #write your gmail and password
+message="yo" #write message
+s.sendmail("rudratheburningfire@gmail.com@gmail.com","rudratheburningfire@gmail.com",message) #write gmail of sender and reciever
 s.quit()
