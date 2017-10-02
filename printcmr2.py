a6613d13730fa47dc5389b8567920f51c4622d02
amrendra singh rathore <31726307+daredevil786@users.noreply.github.com>
2017-10-02 12:16:25 +0530

    Update printcmr2.py

--- printcmr2.py
+++ printcmr2.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-name="cmr"
-print(name)
+name="cmr" # word which we want to  print
+print(name) # condition for print
 print(name*3)
-print(name+"is a big university")
+print(name+"is a big university") #condition for add sentence 
 print(name[0])
 print(name[1])
 print(name[3])
