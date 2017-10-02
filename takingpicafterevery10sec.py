eadd7bb8ffed4f8b5fea04e44309cf06715769cf
amrendra singh rathore <31726307+daredevil786@users.noreply.github.com>
2017-10-02 12:27:57 +0530

    Update takingpicafterevery10sec.py

--- takingpicafterevery10sec.py
+++ takingpicafterevery10sec.py
@@ -5,8 +5,8 @@ a=1
 b=1
 
 while a<15:
-	img="fswebcam -F 3 --fps 25 -r 800x600 " + str(b)+".jpg"
-	os.system(img)
-	time.sleep(10)
+	img="fswebcam -F 3 --fps 25 -r 800x600 " + str(b)+".jpg" #conditon for run camera and it's quality
+	os.system(img) 
+	time.sleep(10) #condition for rum camera after every 10sec
 	a=a+1
 	b=b+1
